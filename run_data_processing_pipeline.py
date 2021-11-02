54cda6e51dbf93d71ccebc75c799df589014abb2
thogge <thogge@bu.edu>
2021-11-02 17:43:36 -0400

    Added comments

--- run_data_processing_pipeline.py
+++ run_data_processing_pipeline.py
@@ -23,11 +23,30 @@ import numpy as np
 import pdb
 
 def check_files_exist(base_dir,field,trans,file_suffixes):
+    """
+    Parameters
+    ----------
+    base_dir : str
+        The directory that will be searched for the files.
+    field : str
+        The associated field of the files.
+    trans : str
+        The name of the transition defined by freq_to_trans_dict.
+    file_suffixes : list
+        A list of the suffixes of the files.
+
+    Returns
+    -------
+    all_files_exist : bool
+        If all of the files that were searched for exist, then True,
+        if not, then False.
+    """
     file_exists = []
     for suff in file_suffixes:
         file_exists.append(os.path.exists(base_dir+"L"+field+"_"+
                                           trans+"_"+suff+".fits")
-    return(file_exists.all())
+    all_files_exist = file_exists.all()
+    return(all_files_exist)
 
 numcores = 1
 overwrite = False
@@ -50,12 +69,21 @@ for o,a in opts:
         print(__doc__)
         sys.exit(2)
 
+#Define directory structure
 rootdir = "/projectnb2/jjgroup/thogge/ramps/"
 cubedir = rootdir+"cubes/"
 template_files_dir = cubedir+"/L23/field/tiles/"
 scripts_dir = rootdir+"scripts/python/"
 
+#List to record fields that have already been processed
 completed_fields = []
+
+"""
+Define all of the line frequencies observed by RAMPS. Create dict to
+connect the frequencies to the transition names. Define the minimum
+and maximum velocities used for spectral cropping. Define the 
+smoothing factors used in output_baselined_data.py
+"""
 freq_list = ["23694","23722","23870","24139","24532","22235","21301",
              "21431","21550","21981","22344","23444","23963"]
 freq_to_trans_dict = {"23694":"NH3_1-1","23722":"NH3_2-2","23870":"NH3_3-3",
@@ -71,13 +99,16 @@ vmaxs = np.array(["140","140","140","140","140","140","140","140",
 downsamples = np.array(["11","11","11","11","11","7","11",
                         "11","7","11","11","7","11"])
 
+#Define the integer-spaced fields observed by RAMPS
 fields = np.concatenate((np.arange(32)+10,np.array([43,45,47]))).astype(str)
 
-
 for field in fields:
+    #Define the subdirectory structure
     field_basedir = cubedir+ "L" + field
     final_dir = field_basedir + "/field/"
     combine_dir = field_basedir + "/field/tiles/"
+    
+    #Check whether the data in this field has been processed
     if "L"+field in completed_fields:
         continue
     else: 
@@ -94,7 +125,11 @@ for field in fields:
         if files_exist.all():
             completed_fields.append("L"+field)
             continue
-
+        
+    """
+    Move all of the raw data from the tile directories to the 
+    combine directory for concatenation.
+    """
     print(field_basedir)
     for subdir in os.listdir(field_basedir):
         tiledir = field_basedir + "/" + subdir
@@ -117,11 +152,15 @@ for field in fields:
                         execute_string = "mv "+fitsfile+" "+combine_dir+files
                     print(execute_string)
                     os.system(execute_string)
-
-
+    """
+    Combine all of the raw tile data for each transition into a single 
+    field. Spectrally crop the resulting data cubes, subtract baselines,
+    create moment maps, and create rms noise maps.
+    """
     os.chdir(combine_dir)
     all_files = os.listdir(combine_dir)
     for i,freq in enumerate(freq_list):
+        #Get the tile files corresponding to individual observations
         freq_file_list = [s for s in all_files if freq+"_MHz_line.fits" in s]
         combine_input_string = ""
         for ff in freq_file_list:
@@ -131,10 +170,14 @@ for field in fields:
                 combine_input_string += ff+","
         combine_file = "L"+field+"_"+freq+"_MHz_line.fits"
         regrid_file = "regrid_"+freq+"_template_map.fits"
+        
+        #Check that the template file for regridding is present
         if not os.path.exists(regrid_file):
             execute_string = "cp "+template_files_dir+regrid_file+" ."
             print(execute_string)
             os.system(execute_string)
+            
+        #Combine the tile files to create a field file
         if not os.path.exists(combine_file):
             execute_string = "python "+scripts_dir+\
                              "combine_raw_fits_data.py -i "+\
@@ -143,11 +186,13 @@ for field in fields:
             print(execute_string)
             os.system(execute_string)
 
+        #Check the rest frequency in the header and change if necessary
         execute_string = "python "+scripts_dir+\
                          "fix_header_restfreq.py -i "+combine_file
         print(execute_string)
         os.system(execute_string)
 
+        #Spectrally crop out the velocities where there is no signal
         crop_file = combine_file[:-5]+"_crop.fits"
         if not os.path.exists(crop_file):
             execute_string = "python "+scripts_dir+\
@@ -157,9 +202,7 @@ for field in fields:
             print(execute_string)
             os.system(execute_string)
 
-        cube_file = "L"+field+"_"+freq_to_trans_dict[freq]+"_cube.fits"
-        mom0_file = "L"+field+"_"+freq_to_trans_dict[freq]+"_mom0.fits"
-        mom1_file = "L"+field+"_"+freq_to_trans_dict[freq]+"_mom1.fits"
+        #Baseline subtract the field data cube and perform moment analysis 
         if not check_files_exist(final_dir,field,freq_to_trans_dict[freq],
                                  ["cube","mom0","mom1"]):
             execute_string = "python "+scripts_dir+\
@@ -170,6 +213,8 @@ for field in fields:
             print(execute_string)
             os.system(execute_string)
 
+        #Make the rms noise map
+        cube_file = "L"+field+"_"+freq_to_trans_dict[freq]+"_cube.fits"
         rms_file = "L"+field+"_"+freq_to_trans_dict[freq]+"_rms.fits"
         if not os.path.exists(rms_file):
             execute_string = "python "+scripts_dir+\
@@ -178,6 +223,7 @@ for field in fields:
             print(execute_string)
             os.system(execute_string)
 
+    #If all of the files were created successfully, add to completed fields
     files_exist = []
     for freq in freq_list:
         if (check_files_exist(combine_dir,field,freq_to_trans_dict[freq],
